49080f22d0faf13e9c7a5470a30069e55aa63ae8
oliverjam <oliverjamesphillips@gmail.com>
2017-03-13 15:28:07 +0000

    Got generic asset handling working

--- src/server.js
+++ src/server.js
@@ -1,42 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 
-function handler (req, res) {
-  var method = req.method;
-  console.log(method);
-  var endpoint = req.url;
-  console.log(endpoint);
-  if (endpoint === '/') {
-  res.writeHead(200, {"Content-Type": "text/html"});
+function handler(req, res) {
+    var endpoint = req.url;
+    if (endpoint === '/') {
+        res.writeHead(200, {"Content-Type": "text/html"});
 
-  fs.readFile(__dirname + '/../public/index.html', function(err, file) {
-    if (err) {
-      console.log(err);
-      return;
+        fs.readFile(__dirname + '/../public/index.html', function(err, file) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.end(file);
+        })
+    } else {
+        var extension = endpoint.split('.')[1];
+        var extensionType = {
+            'html': 'text/html',
+            'css': 'text/css',
+            'js': 'application/javascript',
+            'jpg': 'image/jpeg',
+            'png': 'image/png',
+            'ico': 'image/x-icon'
+        };
+        fs.readFile(__dirname + '/..' + '/public' + endpoint, function(err, file) {
+            res.writeHead(200, {"Content-Type": extensionType[extension]});
+            res.end(file);
+        })
     }
-      res.end(file);
-  })
-}
-  else if (endpoint === '/node') {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('hello node');
-    res.end();
-}
-  else if (endpoint === '/girls') {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('hello girls');
-    res.end();
-}
-  else {
-    res.writeHead(404, {"Content-Type": "text/plain"});
-    res.write('404');
-    res.end();
-  }
-
 }
 
 var server = http.createServer(handler);
 
 server.listen(3000, function() {
-  console.log("Server is listening on port 3000. Ready to accept requests");
+    console.log("Server is listening on port 3000. Ready to accept requests");
 })

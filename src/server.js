b4ac590e5d3c551a0ba59daa836396cbf349f227
pbywater <philippabywater@gmail.com>
2017-03-13 14:55:09 +0000

    adding html file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 var http = require('http');
-
-var message = "hello";
+var fs = require('fs');
 
 function handler (req, res) {
+  var method = req.method;
+  console.log(method);
+  var endpoint = req.url;
+  console.log(endpoint);
+  if (endpoint === '/') {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.write(message);
+
+  fs.readFile(__dirname + '/../public/index.html', function(err, file) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+      res.end(file);
+  })
+}
+  else if (endpoint === '/node') {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write('hello node');
   res.end();
+}
+else if (endpoint === '/girls') {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.write('hello girls');
+res.end();
+}
 }
 
 var server = http.createServer(handler);

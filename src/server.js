7e601055db72dfdd69c8a27b69ce5c22dc2def5a
pbywater <philippabywater@gmail.com>
2017-03-13 14:38:08 +0000

    handler working

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = "hello";
+
+function handler (req, res) {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(message);
+  res.end();
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests");
